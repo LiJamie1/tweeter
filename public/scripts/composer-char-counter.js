26888a57cbe98051247342c615cb314ff62eca0e
Jamie Li <lijamie19828@gmail.com>
2021-11-16 23:44:19 +0000

    updated composer-char-counter.js fixed misunderstanding >= now >, add maxCharLength as a constant for readability. header.css changed to use a newly made id for the header with a users photo and name. index.html now has temporary content, classes/ids have yet to be defined

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,17 +2,18 @@ $(document).ready(function() {
   console.log("ready");
   // event handler to textarea form inside new-tweet
   $("#tweet-text").on("input", function() {
+    const maxCharLength = 140
     const charLength = $(this).val().length;
     const $tweetForm = $(this).closest("form");
     const $counter = $tweetForm.find(".counter");
+
     // edits the html to update character limit
-    $counter.html(140 - charLength);
-    console.log(charLength)
-    if (charLength >= 140) {
+    $counter.html(maxCharLength - charLength);
+    
+    if (charLength > 140) {
       $counter.css("color", "red");
-    } else {
-      $counter.css("color", "#545149");
-    }
+    } 
+
   });
 });
 
