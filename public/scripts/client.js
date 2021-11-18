21d165a3c254a0d39ce9f361bb7628fec3ac035e
Jamie Li <lijamie19828@gmail.com>
2021-11-18 01:19:20 +0000

    removed hard coded test code, wrote in loadTweet function with ajax get request

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,34 +4,15 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// fake/temp data
-// const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png"
-//       ,
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd" },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   }
-// ]
-// renderTweets(data);
-
 $(document).ready(function() {
+
+  //load tweet
+  const loadTweet = function() {
+    $.ajax("http://localhost:8080/tweets", {method: "GET"})
+      .done((data) => {
+        renderTweets(data)
+      })
+  }
 
   //submit handler & post request
   $("#form-tweet").submit(function( event ) {
